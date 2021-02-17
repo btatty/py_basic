6892a280714aa24cfbe8b4daf4aa3c8463805bb1
btatty <78550602+btatty@users.noreply.github.com>
2021-02-17 17:19:17 +0200

    fixes

--- hw5/password_gen.py
+++ hw5/password_gen.py
@@ -2,48 +2,47 @@ import string
 import random
 
 
-def generate_password_easy():
-    chars = string.ascii_lowercase
-    size = 8
-    password = ''
-    for x in range(size):
-        password += random.choice(chars)
-    return password
-
-
-def generate_password_medium():
-    chars = string.ascii_lowercase + string.ascii_uppercase + string.digits
-    size = 8
-    password = ''
-    for x in range(size):
-        password += random.choice(chars)
-    return password
-
-
-def generate_password():
-    chars = string.ascii_lowercase + string.ascii_uppercase + string.digits + string.punctuation
-    size = random.randint(8, 16)
-    password = ''
-    for x in range(size):
-        password += random.choice(chars)
-    return password
-
-
-while True:
-    menu = input(
+def main():
+    choice = input(
         "1. Сгенерировать простой пароль (только буквы в нижнем регистре, 8 символов)\n"
         "2. Сгенерировать средний пароль (любые буквы и цифры, 8 символов)\n"
         "3. Сгенерировать сложный пароль (минимум 1 большая буква, 1 маленькая, 1 цифра и 1 спец-символ, длина от 8 до 16 символов)\n"
-        "4. Выход \n"
         "Сделайте выбор и нажмите Enter: "
     )
-    if menu == "1":
-        print(generate_password_easy())
-    elif menu == "2":
-        print(generate_password_medium())
-    elif menu == "3":
-        print(generate_password())
-    elif menu == "4":
-        break
-    else:
-        print("Повторите выбор!")
+    if choice == "1":
+        print(gen_password(string.ascii_lowercase))
+    elif choice == "2":
+        print(gen_password(string.ascii_letters + string.digits))
+    elif choice == "3":
+        print(gen_strong_password())
+
+
+def gen_password(chars, length=8):
+    password = ""
+    for i in range(length):
+        password += random.choice(chars)
+    return password
+
+
+def gen_strong_password():
+    length = random.randint(8, 16)
+    password = gen_password(string.digits + string.ascii_letters + string.punctuation, length)
+
+    counter_d = counter_u = counter_l = counter_s = 0
+    for char in password:
+        if char.isdigit():
+            counter_d += 1
+        elif char.isupper():
+            counter_u += 1
+        elif char.islower():
+            counter_l += 1
+        elif not char.isspace():
+            counter_s += 1
+
+    if counter_d and counter_u and counter_l and counter_s:
+        return password
+    return gen_strong_password()
+
+
+if __name__ == '__main__':
+    main()
--- hw5/reg_form.py
+++ hw5/reg_form.py
@@ -1,13 +1,28 @@
+import string
+import random
+
+
+def main():
+    phone_finished = correct_phone()
+    email_finished = correct_email()
+    password_finished = correct_password()
+    checking(phone_finished, email_finished, password_finished)
+
+
 def correct_email():
-    if email.count('@') > 1 or email.count('@') == 0:
-        return False, 'Неверное количество знаков @'
+    email = input('Введите email')
     [login, domain] = email.split('@')
+    if email.count('@') > 1 or email.count('@') == 0:
+        print('Неверное количество знаков @')
+        correct_email()
     if len(login) < 6:
-        return False, 'Логин должен содержать 6 и больше символов'
+        print('Логин должен содержать 6 и больше символов')
+        correct_email()
     return email
 
 
 def correct_phone():
+    phone = input('Введите номер телефона')
     digits = ''
     for char in phone:
         if char.isdigit():
@@ -16,44 +31,47 @@ def correct_phone():
     if len(digits) >= 9:
         n_phone = '380' + digits[-9:]
     else:
-        return False, 'Неправильный формат'
+        print('Неправильный формат')
+        correct_phone()
     return n_phone
 
 
-def has_digits():
-    return any(char.isdigit() for char in password)
-
-
-def has_uppers():
-    return any(char.isupper() for char in password)
-
-
-def has_lowers():
-    return any(char.islower() for char in password)
-
-
 def correct_password():
+    password = input('Введите пароль')
     if len(password) < 8:
-        return False, 'Пароль должен содержать минимум 8 символов'
-    if has_digits() and has_lowers() and has_uppers() == 0:
-        return 'Пароль должен содержать минимум 1 букву в верхнем регистре, 1 в нижнем и 1 цифру'
+        print('Пароль должен содержать минимум 8 символов')
+        correct_password()
+    counter_d = counter_u = counter_l = counter_s = 0
+    for char in password:
+        if char.isdigit():
+            counter_d += 1
+        elif char.isupper():
+            counter_u += 1
+        elif char.islower():
+            counter_l += 1
+        else:
+            counter_s += 1
+
+    if (counter_d and counter_u and counter_l) == 0:
+        print('Пароль должен содержать минимум 1 букву в верхнем регистре, 1 в нижнем и 1 цифру')
+        correct_password()
+    new_password = input('Повторите пароль')
+    if new_password != password:
+        print('Пароли не совпадают')
+        correct_password()
     return password
 
 
-def ask_password():
-    if new_password == password:
-        print('Вы успешно прошли регестрацию')
-    else:
-        print('Пароли не совпадают')
-    return new_password
+def checking(phone_finished, email_finished, password_finished):
+    count_password = len(password_finished)
+    count_password = count_password * "*"
+    print(
+        f"Поздравляем с успешной регистрацией!\n\
+        Ваш номер телефона: {phone_finished}\n\
+        Ваш email: {email_finished}\n\
+        Ваш пароль: {count_password}"
+    )
 
 
 if __name__ == "__main__":
-    phone = input('Введите номер телефона')
-    print(correct_phone())
-    email = input('Введите email')
-    print(correct_email())
-    password = input('Введите пароль')
-    print(correct_password())
-    new_password = input('Повторите пароль')
-    print(ask_password())
+    main()
