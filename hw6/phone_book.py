b94e6263198ccea8004985700a8f4704d481f8c9
btatty <78550602+btatty@users.noreply.github.com>
2021-03-01 13:56:08 +0200

    add

---
+++ hw6/phone_book.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+
+
+def main():
+    with open("phone_book.txt") as f_in:
+        with open("edited_phone_book.txt", "a") as f_out:
+            count = 1
+            for line in f_in.readlines():
+                name = "".join(char for char in line if char.isalpha()).lower()
+                if name.startswith("m") or name.endswith("a"):
+                    phone = "".join(char for char in line if char.isdigit())
+                    phone = "+380" + phone[-9:]
+                    print(f"{count}. {phone} - {name.title()}", file=f_out)
+                    count += 1
+
+
+if __name__ == "__main__":
+    main()
